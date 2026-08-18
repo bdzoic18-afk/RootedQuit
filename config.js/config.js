e946f5eae37d564c7a5f7fd936f55fef9df24eb0
bdzoic18-afk <bdzoic18@gmail.com>
2026-08-18 10:10:57 -0700

    Delete config.js/config.js

--- config.js/config.js
+++
@@ -1,3 +0,0 @@
-// Set automatically after the private Cloudflare Worker is deployed.
-// This file intentionally contains no secrets.
-window.ROOTED_QUIT_NOTION_ENDPOINT = "https://rooted-quit-journal.benton-dzoic.workers.dev";
